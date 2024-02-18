8533dcfc50c0f1696308618b763559729488ec06
VelocityOfStuff-Cox <dcox71943@gmail.com>
2024-02-17 23:27:00 -0600

    assignment completion and advancement

--- Project1/Project1/pb1.Source.cpp
+++ Project1/Project1/pb1.Source.cpp
@@ -1,9 +1,9 @@
 /*
 * Developer: Isabella Cox
 * Contact: velocityofstuff1@gmail.com
-* Version: 1.1
+* Version: 1.3
 * Creation Date: 1/20/2024
-* Version Creation Date: 1/25/2024
+* Version Creation Date: 2/3/2024
 * Written with Visual Studio, assistance from learncpp.com, and cplusspluss.com for function syntax
 */
 
@@ -14,20 +14,20 @@
 #include <array>
 #include <algorithm>
 
-struct Entity
+struct Object
 {
 	std::string name;
 	int strength{ 0 };
 	int health{ 0 };
-	std::string status{ "<ALIVE>" };
+	bool dead{ 0 };
 };
 
-void displayBattle(Entity player, std::vector<Entity> monsters, std::mt19937 mt);
-void playerAttack(Entity player, std::vector<Entity> monsters, std::mt19937 mt);
-void monsterAttack(Entity player, std::vector<Entity> monsters, std::mt19937 mt);
-int attack(Entity attacker, std::mt19937 mt);
-int defend(Entity defender, int damage);
-int heal(Entity creature, std::mt19937 mt);
+void displayBattle(Object player, std::vector<Object> monsters);
+void playerAttack(Object& player, Object& monsters, std::mt19937& mt);
+void monsterAttack(Object& player, Object& monster, std::mt19937& mt);
+int attack(Object& attacker, std::mt19937& mt);
+int defend(Object& defender, int damage);
+int heal(Object& creature, std::mt19937& mt);
 
 int main()
 {
@@ -37,7 +37,7 @@ int main()
 	std::normal_distribution randHp{ 30.0, 5.0 };
 
 	//Player Character with random strength and health values
-	Entity player{};
+	Object player{};
 	player.name = "Gongalor";
 	player.strength = std::max(1, int(randStr(mt)));
 	player.health = std::max(1, int(randHp(mt)));
@@ -45,13 +45,13 @@ int main()
 	//creates one to three entites called "monsters" for the player to fight
 	std::uniform_int_distribution ud{ 1, 3 };
 	int spawn{ ud(mt) };
-	std::vector<Entity> monsters;
+	std::vector<Object> monsters;
 	for (int i{ 0 }; i < spawn; i++)
 	{
 		//intitializes the monster for the current itteration of the loop
 		//strength and health are random like the player
 		std::string monsterName{ "Monster " + std::to_string(i + 1) };
-		monsters.push_back(Entity());
+		monsters.push_back(Object());
 		monsters.back().name = monsterName;
 		monsters.back().strength = std::max(1, int(randStr(mt)));;
 		monsters.back().health = std::max(1, int(randHp(mt)));
@@ -59,11 +59,63 @@ int main()
 
 	std::cout << "Fight the Monsters" << "\n" << std::endl;
 
+	//Game loop
 	while (player.health > 0 && monsters.size() > 0)
 	{
-		displayBattle(player, monsters, mt);
+		displayBattle(player, monsters);
+
+		//initializes the user input variable
+		int userInput{ 0 };
+		std::cin >> userInput;
+
+		//Player's turn 
+		switch (userInput)
+		{
+		case 1:
+
+			std::cout << "Enter the number of the monster you wish to attack" << std::endl;
+
+			std::cin >> userInput;
+
+			//Input validated combat Player wastes turn on invalid targets
+			if (userInput > 0 && userInput <= monsters.size() && !monsters.at(userInput - 1).dead)
+			{
+				playerAttack(player, monsters.at(userInput - 1), mt);
+			}
+			else
+			{
+				std::cout << "Please select a valid target" << std::endl;
+			}
+			break;
+		case 2:
+			//heals player
+			player.health = heal(player, mt);
+			break;
+		default:
+			std::cout << "Please input an appropriate value." << std::endl;
+			break;
+		}
+
+		for (int i{ 0 };i < monsters.size(); i++)
+		{
+			//monster will randomly attack or heal
+			std::bernoulli_distribution chance(0.75);
+			bool attackHeal{ chance(mt) };
+			if (attackHeal == 1)
+			{
+				monsterAttack(player, monsters.at(i), mt);
+			}
+			else
+			{
+				monsters.at(i).health = heal(monsters.at(i), mt);
+			}
+		}
+
+		system("PAUSE");
+		system("CLS");
 	}
 
+	//possible game ove conditions
 	if (player.health <= 0)
 	{
 	std::cout << "You fell to the monsters :( Thank you for playing!" << std::endl;
@@ -83,7 +135,7 @@ int main()
 
 }
 
-void displayBattle(Entity player, std::vector<Entity> monsters, std::mt19937 mt)
+void displayBattle(Object player, std::vector<Object> monsters)
 {
 	std::cout << player.name
 		<< "\nHealth: " << player.health
@@ -92,83 +144,44 @@ void displayBattle(Entity player, std::vector<Entity> monsters, std::mt19937 mt)
 
 	for (int i{ 0 }; i < monsters.size(); i++)
 	{
-		if (monsters[i].health > 0)
+		if (monsters.at(i).health > 0)
 		{
-			std::cout << i + 1 << ". " << monsters[i].name
-				<< "\nHealth: " << monsters[i].health
-				<< "\nStrength: " << monsters[i].strength 
+			//prints living monsters
+			std::cout << i + 1 << ". " << monsters.at(i).name
+				<< "\nHealth: " << monsters.at(i).health
+				<< "\nStrength: " << monsters.at(i).strength 
 				<< "\n" << std::endl;
 		}
 		else
 		{
-		std::cout << i + 1 << ". " << monsters[i].name
-			<< "\nHealth: " << monsters[i].status
-			<< "\nStrength: " << monsters[i].strength 
-			<< "\n" << std::endl;
+			//prints dead monsters
+			std::cout << i + 1 << ". " << monsters.at(i).name
+				<< "\nHealth: <DEAD>"
+				<< "\nStrength: " << monsters.at(i).strength 
+				<< "\n" << std::endl;
 		}
 			
 	}
-
-	playerAttack(player, monsters, mt);
 	
-	monsterAttack(player, monsters, mt);
-}
-
-void playerAttack(Entity player, std::vector<Entity> monsters, std::mt19937 mt)
-{
 	std::cout << "Chose an action by typing it's associated number: " << "\n"
 		<< "\t1. Attack" << "\n"
 		<< "\t2. Heal" << "\n"
 		<< std::endl;
-
-	int userInput{ 0 };
-	std::cin >> userInput;
-
-	switch (userInput)
-	{
-	case 1:
-
-		std::cout << "Enter the number of the monster you wish to attack" << std::endl;
-
-		std::cin >> userInput;
-
-		if (userInput > 0 && userInput <= monsters.size())
-		{
-			//the casting syntax was a suggested correection by viual studio
-			monsters[static_cast<std::vector<Entity, std::allocator<Entity>>::size_type>(userInput) - 1].health = defend(monsters[userInput - 1], attack(player, mt));
-		}
-		else
-		{
-			std::cout << "Please select a valid target" << std::endl;
-		}
-		break;
-	case 2:
-		player.health = heal(player, mt);
-		break;
-	default:
-		std::cout << "Please input an appropriate value." << std::endl;
-		break;
-	}
 }
 
-void monsterAttack(Entity player, std::vector<Entity> monsters, std::mt19937 mt)
+void playerAttack(Object& player, Object& monster, std::mt19937& mt)
 {
-	for (int i{ 0 }; i < monsters.size(); i++)
-	{
-		std::bernoulli_distribution chance(0.75);
-		bool attackHeal{ chance(mt) };
-		if (attackHeal == 1)
-		{
-			player.health = defend(player, attack(monsters[i], mt));
-		}
-		else
-		{
-			monsters[i].health = heal(monsters[i], mt);
-		}
-	}
+	
+	monster.health = defend(monster, attack(player, mt));
+	
 }
 
-int attack(Entity attacker, std::mt19937 mt)
+void monsterAttack(Object& player, Object& monster, std::mt19937& mt)
+{
+	player.health = defend(player, attack(monster, mt));
+}
+
+int attack(Object& attacker, std::mt19937& mt)
 {
 	double strength{ double(attacker.strength) };
 	std::normal_distribution d(strength, 2.0);
@@ -178,21 +191,20 @@ int attack(Entity attacker, std::mt19937 mt)
 	return damage;
 }
 
-int defend(Entity defender, int damage)
+int defend(Object& defender, int damage)
 {
 	defender.health -= damage;
 	std::cout << defender.name << " has taken " << damage << " damage.\n" << std::endl;
-	std::cout << defender.health << std::endl;
 
 	if (defender.health <= 0)
 	{
-		defender.status = "<DEAD>";
+		defender.dead = 1;
 	}
 
 	return defender.health;
 }
 
-int heal(Entity creature, std::mt19937 mt)
+int heal(Object& creature, std::mt19937& mt)
 {
 	double strength{ double(creature.strength) };
 	std::normal_distribution d(strength * 2, 3.0);
